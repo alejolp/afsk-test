ae94c5968f51c2ffc9322c1cec8cb636f320a33e
Alejandro Santos <alejolp@gmail.com>
2016-12-11 14:56:55 +0100

    More changes, now it works with both 01 and 02, pre-emph and no pre-emph

--- fskcorrwave.py
+++ fskcorrwave.py
@@ -26,7 +26,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-def butter_lowpass_filter2(data, cutoff, fs, order=9):
+def butter_lowpass_filter2(data, cutoff, fs, order=2):
     A, B = chebyshev.calc(cutoff / (0.5 * fs), 0, 0.0, order)
     return chebyshev.filter(data, A, B, order)
 
@@ -35,9 +35,19 @@ def main():
     if "audio01.wav" in sys.argv[1]:
         pos_start = 267569
         pos_end = 309673
+        wth1 = 1e7
+        wth2 = -1e7
+        th1 = 1e7
+        th2 = -1e7
+        phcorr = 0
     elif "audio02.wav" in sys.argv[1]:
         pos_start = 325400
         pos_end = 363900
+        wth1 = 100000
+        wth2 = -250000
+        th1 = 6e6
+        th2 = -3.5e6
+        phcorr = -20
 
     w = wave.open(sys.argv[1], 'rb')
 
@@ -59,7 +69,7 @@ def main():
     bps_rate = 1200
     f1 = 1200
     f2 = 2200
-    fcut = 700 # Hz for LPF
+    fcut = 600 # Hz for LPF
 
     L = [-math.cos(2 * math.pi * f1 * (x) / samp_rate) + math.cos(2 * math.pi * f2 * (x) / samp_rate) for x in range(2 * samp_rate / bps_rate)]
     dd = L.index(max(L))
@@ -92,31 +102,81 @@ def main():
 
     # Schmitt trigger/hysteresis
 
-    F = []
-    if False:
-        th1 = 65000
-        th2 = -65000
+    if True:
+        F = []
+        vh = th1
+        vl = th2
+        last = vh
+        for x in E:
+            if x > th1:
+                last = vh
+            elif x < th2:
+                last = vl
+            F.append(last)
+    elif False:
+        hyst_hist = [0] * (samples_per_bit * 16)
+        hyst_hist_idx = 0
+        F = []
+        vh = 0.0
+        vl = 0.0
+        last = 0
+        for x in E:
+            hyst_hist[hyst_hist_idx % len(hyst_hist)] = x
+            hyst_hist_idx += 1
+            L = sorted(hyst_hist)
+            th1 = vh = L[9 * len(L) / 10] * 0.75
+            th2 = vl = L[1 * len(L) / 10] * 0.75
+            if x > th1:
+                last = 1
+            elif x < th2:
+                last = 0
+            F.append(last * wth1)
     else:
-        th1 = 4e6
-        th2 = -4e6
-    vh = th1
-    vl = th2
-    last = vh
-    for x in E:
-        if x > th1:
-            last = vh
-        elif x < th2:
-            last = vl
-        F.append(last)
+        F = []
+        G = []
+
+        eta = 1.6
+
+        eta_fast = eta / 10.0
+        mean_fast = 0.0
+        median_fast = 0.0
+
+        eta_slow = eta / 100.0
+        mean_slow = 0.0
+        median_slow = 0.0
+
+        last = 0
+        for x in E:
+            mean_fast += eta_fast * (x - mean_fast)
+            #median_fast += eta_fast * math.copysign(1.0, x - median_fast)
+
+            mean_slow += eta_slow * (x - mean_slow)
+            #median_slow += eta_slow * math.copysign(1.0, x - median_slow)
+
+            d = mean_fast - mean_slow
+            F.append(d)
+
+            if d >= wth1:
+                last = wth1
+            elif d < wth2:
+                last = wth2
+
+            G.append(last)
 
     #plt.plot(A, label='A')
-    plt.plot(B, label='B')
+    
+    #plt.plot(B, label='B')
     plt.plot(E, label='E')
-    plt.plot(F, label='F')
+    #plt.hist(E, bins=50, log=True, label='E_hist')
+    #plt.plot(F, label='F')
+    #plt.hist(F[1200:18500], bins=50, log=True, label='F_hist')
+    #plt.hist(G, bins=50, label='G_hist')
+    #plt.plot(G, label='G')
     #plt.plot([vh * math.sin(2 * math.pi * 1200 * x / samp_rate) for x in range(len(F))], label='Clock')
-    plt.plot([(vh * (int((x +20) * 1200 / samp_rate) % 2)) for x in range(len(F))], 
-        label='Clock', color='m')
+    
+    plt.plot([(wth1 * (int((x + phcorr) * 1200 / samp_rate) % 2)) for x in range(len(F))], label='Clock', color='m')
 
+    #plt.ylim(-5, 5)
     plt.grid()
     plt.legend()
     plt.show()
